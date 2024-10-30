3bff8d5a46dbbf56a1363f3e5b6414b6678ba505
MozartofCode <bertan.berker@gmail.com>
2024-10-30 18:17:53 -0400

    Created a pydantic class for better input/output for the agent

--- poker_agent.py
+++ poker_agent.py
@@ -5,6 +5,7 @@
 #
 
 from crewai import Agent, Task, Crew
+from pydantic import BaseModel
 
 
 # Agent1 - Evaluate Poker Hand + community cards to evaluate the chances of winning
@@ -13,7 +14,40 @@ from crewai import Agent, Task, Crew
 # Agent4 - Return the move-bet to the environment
 
 
-class PokerAgent(Agent):
+
+# Created the class for this format for easier output/input from and for the agents
+
+class Move(BaseModel):
+    agentBigBlind: bool
+    gameTurn: str
+    pot: int
+    agentBet: int
+    playerBet: int
+    playerMoney: int
+    agentMoney: int
+    communityCards: list
+    playerHand: list
+    agentHand: list
+
+
+
+
+class PokerAgent():
     def __init__(self, money):
         self.money = money
+    
+    def play_pre_flop(self):
+        return
+    
+    def play_flop(self):
+        return
+
+    def play_turn(self):
+        return
+
+    def play_river(self):
+        return
+    
+
+
 
