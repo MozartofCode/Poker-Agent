eecb9db3957badd59e7c6a92bd856264fde12b05
MozartofCode <bertan.berker@gmail.com>
2024-11-01 17:47:51 -0400

    Pre-flop game logic is implemented

--- gameplay.py
+++ gameplay.py
@@ -111,7 +111,7 @@ class PokerEnv:
             "agentHand": self.agent_hand
         }
 
-
+    # :return: winner of the round as 'agent' or 'bot'
     def play_round(self):
 
         # Increasing the blinds (if necessary)
@@ -120,12 +120,27 @@ class PokerEnv:
         if self.hand_count % 50 == 0:
             self.big_blind += 25
             self.small_blind += 50
-        
-        
+
         # Monte Carlo Bot is the Big Blind
         if self.blind_monte:
+                
+            self.agent_bet += self.small_blind
+            self.monte_carlo_bot_bet += self.big_blind
 
             # Pre-flop: Agent starts first
+
+            while self.agent_bet != self.monte_carlo_bot_bet:
+                agent_move = self.agent.play_pre_flop(self.get_current_env())
+                if agent_move == "fold":
+                    return "bot"
+                elif agent_move == "call":
+                    self.agent_bet = self.monte_carlo_bot_bet
+                    bot_move = self.monte_carlo_bot.play_pre_flop(self.get_current_env())
+                    if bot_move == "fold":
+                        return "agent"
+                    elif bot_move == "check":
+                        break
+            
 
             # After Flop: Monte Carlo Bot starts first
             self.deal_flop()
@@ -136,6 +151,8 @@ class PokerEnv:
             # After River: Monte Carlo Bot starts first
             self.deal_river()
 
-
+        # Agent is the Big Blind
+        else:
+            return
+        
         self.big_monte = not self.big_monte
-    
--- main.py
+++ main.py
@@ -5,32 +5,13 @@
 from gameplay import PokerEnv
 
 
-def choose_winner(rl_bot, monte_carlo_bot):
-
-    if rl_bot.money > monte_carlo_bot.money:
-        return "RL Bot"
-    elif rl_bot.money < monte_carlo_bot.money:
-        return "Monte Carlo Bot"
-
-
 def main():
     
-    env = PokerEnv()
+    game = PokerEnv()
 
-    # Is Monte Carlo Bot the Big Blind
-    isMonteBB = True
-
-    while  env.rl_bot.money > 0 and env.monte_carlo_bot.money > 0:
+    while  game.agent.money > 0 and game.monte_carlo_bot.money > 0:
+        game.play_round(True)
         
-        if isMonteBB:
-            env.play_round(True)
-            isMonteBB = False
-        else:
-            env.play_round(False)
-            isMonteBB = True
-
-    print("The winner is:" + choose_winner(env.rl_bot, env.monte_carlo_bot))
-
 
 if __name__ == "__main__":
     main()
