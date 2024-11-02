bc97d1b41f9bf27a253d5ab369dfb4b26613fa41
MozartofCode <bertan.berker@gmail.com>
2024-11-02 16:45:12 -0400

    Tasks are complete

--- poker_agent.py
+++ poker_agent.py
@@ -23,19 +23,6 @@ os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
 search_tool = SerperDevTool()
 scrape_tool = ScrapeWebsiteTool()
 
-# Created the class for this format for easier output/input from and for the agents
-
-class Move(BaseModel):
-    agentBigBlind: bool
-    gameTurn: str
-    pot: int
-    agentBet: int
-    playerBet: int
-    playerMoney: int
-    agentMoney: int
-    communityCards: list
-    agentHand: list
-
 
 # AGENTS
 evaluate_situation = Agent(
@@ -71,51 +58,39 @@ bluff_master = Agent(
 )
 
 
-
 # TASKS
+
 analyze = Task(
     description=(
-        ""
+        "Analyze the current situation in Texas hold'em poker game and evaluate the chances of winning "
+        "based on the known information. The known information includes the {communityCards}, {agentHand}, "
+         "{agentMoney}, {playerMoney}, {pot}, {gameTurn}, {agentBet} and {playerBet}"
     ),
     expected_output=(
-        ""
+        "The probability in the precentage of winning the game based on the current situation in addition"
+        " to a paragraph explaining the situation and the reason behind the calculation"
     ),
     agent= evaluate_situation
 )
 
 make_move = Task(
-    description="Find a venue in {event_city} ",
-    expected_output="",
-    human_input=True,
-    output_json= Move,
-    output_file="venue_details.json",  
-    agent= decide_move
-)
-
-
-bet = Task(
-    description="Find a venue in {event_city} ",
-    expected_output="",
-    human_input=True,
-    output_json= Move,
-    output_file="venue_details.json",  
-    agent= decide_bet
-)
-
-decision = Task(
-        description="Find a venue in {event_city} ",
-    expected_output="",
-    human_input=True,
-    output_json= Move,
-    output_file="venue_details.json",  
-    agent= return_decision
+    description=(
+        "Make a move in the Texas Hold'em Poker game based on the current situation and the analysis of the situation, "
+        " the move can be raising, calling, checking or folding and based on the positional play, the player can bluff as well based on the known "
+        " information. The known information includes the {communityCards}, {agentHand}, {agentMoney}, {playerMoney}, {pot}, {gameTurn}, {agentBet} and {playerBet}",
+    ),
+    expected_output=(
+        "The move that the agent should make in the game in the format of a string, the move can be raising, calling, checking or folding"
+        "The output should be in the format: 'check', 'call', 'fold' or 'raise-$50' (where $50 is the amount to raise based on the current situation)",
+    ), 
+    agent=[decide_move, bluff_master]
 )
 
 
 # Define the crew with agents and tasks
 poker_crew = Crew(
     agents=[evaluate_situation, decide_move],
-    tasks=[analyze, make_move, bet],
+    tasks=[analyze, make_move],
     manager_llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7),
     process=Process.hierarchical,
     verbose=True
